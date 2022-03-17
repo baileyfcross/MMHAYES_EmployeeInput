c0410f7dad546d85deb2ab0c7ed9aceece45152b
baileyfcross <bf02cros@siena.edu>
2022-03-17 01:45:45 -0400

    updated so it send the object data to the screen

--- functions.js
+++ functions.js
@@ -1,17 +1,16 @@
-
   function onSubmit(form){
     var unstrJsData = $(form).serializeArray();
     var jsdata = JSON.stringify($(form).serializeArray());
 
-
     $.ajax({
-        //url: 'submit.html',
+        url: 'submit.html',
         dataType: 'JSON',
-        type: 'POST',
+        type: 'post',
         data: jsdata,
         //processData: false,
         success: function(data, textStatus, jQxhr){
             $('.eFormDiv').text(jsdata);
+            console.log(jsdata);
         },
         error: function(jqXhr, textStatus, errorThrown, data){
             //console.log(errorThrown);
