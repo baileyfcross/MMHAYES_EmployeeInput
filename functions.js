f08a6301b5d0a4d9d6b6b2e3828bff40c9076c59
baileyfcross <bf02cros@siena.edu>
2022-03-18 10:26:06 -0400

    updated so we have more json like returned

--- functions.js
+++ functions.js
@@ -1,22 +1,41 @@
   function onSubmit(form){
-    var unstrJsData = $(form).serializeArray();
-    var jsdata = JSON.stringify($(form).serializeArray());
+    var array = $(form).serializeArray();
 
+    //var test = array[1].value;
+    var nameArray = new Array();
+    var valueArray = new Array();
+    var totalArray = new Array();
+/*
+    for (var i = 0; i < array.length; i++) {
+      nameArray.push(array[i].name);
+      valueArray.push(array[i].value);
+      //totalArray.push(array[i].name:array[i].value);
+    }
+*/
+    //totalArray.push({nameArray:valueArray});
+
+    //var test3 = array.map((array.name)=>array.value);
+
+    var spaced = JSON.stringify({employeeInfo:array},null,4);
+    //var jsdata = JSON.stringify($(form).serializeArray());
+    //console.log(array);
+    //console.log(spaced);
+    //$('.eFormDiv').text(spaced);
+    $('.eFormDiv').text(spaced);
+    //$('.eFormDiv').text(valueArray);
+    console.log(spaced);
     $.ajax({
         url: 'submit.html',
         dataType: 'JSON',
         type: 'post',
-        data: jsdata,
+        data: spaced,
         //processData: false,
         success: function(data, textStatus, jQxhr){
-            $('.eFormDiv').text(jsdata);
-            console.log(jsdata);
+            //$('.eFormDiv').text(spaced);
         },
-        error: function(jqXhr, textStatus, errorThrown, data){
-            //console.log(errorThrown);
-            $('.eFormDiv').text(jsdata);
-            console.log(jsdata);
-
+        error: function(jqXhr, textStatus, errorThrown){
+            console.log(errorThrown);
+            //$('.eFormDiv').text(spaced);
         }
     });
     return false;
